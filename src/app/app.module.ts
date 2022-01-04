cddb6ae77d62dccf22f90540d173e3f615d6a5d7
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2022-01-04 16:46:39 +0100

    Add empty shipping component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule
